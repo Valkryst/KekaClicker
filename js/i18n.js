234bc68d2f9f5077e0ba2b3492db3583589caaca
Valkryst <valkryst@valkryst.com>
2025-11-20 12:42:22 -0400

    Fix issue where `i18n.js` wasn't localizing the `<title>` element

--- js/i18n.js
+++ js/i18n.js
@@ -2,16 +2,30 @@
  * This script should be declared at the end of an HTML document. It will automatically check all text elements for
  * textContent containing "__MSG_localizationKey__" placeholders, and auto-replace them with their localized strings.
  */
-const regex = /__MSG_([A-Za-z0-9_]+)__/g;
-const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
 
+function localize(text) {
+    if (!text) {
+        return "Unknown";
+    }
+
+    return text.replace(/__MSG_([A-Za-z0-9_]+)__/g, (match, key) => {
+        return chrome.i18n.getMessage(key)?.trim() || "Unknown";
+    });
+}
+
+// Localize <title>.
+const titleElement = document.querySelector("title");
+if (titleElement?.textContent) {
+    titleElement.textContent = localize(titleElement.textContent);
+}
+
+// Localize text nodes in the document body.
+const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
 let node;
 while (node = walker.nextNode()) {
     if (!node.nodeValue) {
         continue;
     }
 
-    node.nodeValue = node.nodeValue.replace(regex, (_, key) => {
-        return chrome.i18n.getMessage(key)?.trim() || "Unknown";
-    });
+    node.nodeValue = localize(node.nodeValue);
 }
